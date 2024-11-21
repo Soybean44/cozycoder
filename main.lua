352f8969f2474c7ca8d07858f02a3e74183c329b
Sovereign Shahid <sovereignshahid@gmail.com>
2024-11-21 09:04:00 -0700

    removed urutora

--- main.lua
+++ main.lua
@@ -1,3 +1,34 @@
+urutora = require("urutora.urutora")
+local u = urutora:new()
+
+function love.load()
+	local clickMe = u.text()
+	u:add(clickMe)
+end
+
+function love.update(dt)
+	u:update(dt)
+end
+
 function love.draw()
-	love.graphics.rectangle("fill", 100, 100, 100, 100)
+	u:draw()
+end
+
+function love.mousepressed(x, y, button)
+	u:pressed(x, y)
+end
+function love.mousemoved(x, y, dx, dy)
+	u:moved(x, y, dx, dy)
+end
+function love.mousereleased(x, y, button)
+	u:released(x, y)
+end
+function love.textinput(text)
+	u:textinput(text)
+end
+function love.keypressed(k, scancode, isrepeat)
+	u:keypressed(k, scancode, isrepeat)
+end
+function love.wheelmoved(x, y)
+	u:wheelmoved(x, y)
 end
